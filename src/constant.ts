831b5f1e8c7b2aff91978f73d34bbe643c773e2f
hayakei <hayakei.kiri@gmail.com>
2022-03-16 01:19:40 +0700

    -

---
+++ src/constant.ts
@@ -0,0 +1,11 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export const METAWARDEN_ADDRESS = "0xa5fdb0822bf82de3315f1766574547115e99016f";
+export const TOFU_MARKET_ADDRESS = "0x449d05c544601631785a7c062dcdff530330317e";
+export const NFTRADE_MARKET_ADDRESS =
+    "0xcfb6ee27d82beb1b0f3ad501b968f01cd7cc5961";
+export const WBNB_ADDRESS = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+
+export let ZERO = BigInt.fromI32(0);
+export let ONE = BigInt.fromI32(1);
+
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,12 +1,15 @@
 import { Transfer } from "../generated/MetaWarden/MetaWarden";
 import { MetaWarden, Owner, Stat } from "../generated/schema";
 import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { ZERO, ONE } from "./constant";
 
 export const getOwner = (ownerAddress: Address, stat: Stat): Owner => {
     let owner = Owner.load(ownerAddress.toHexString());
     if (owner === null) {
         owner = new Owner(ownerAddress.toHexString());
-        stat.ownerCount = stat.ownerCount.plus(BigInt.fromI32(1));
+        owner.assetCount = ZERO;
+
+        stat.ownerCount = stat.ownerCount.plus(ONE);
     }
     return owner as Owner;
 };
@@ -16,9 +19,9 @@ export const removeOwner = (ownerAddress: Address, stat: Stat): void => {
     if (owner !== null) {
         owner.unset(ownerAddress.toHexString());
 
-        let tmp = stat.ownerCount.minus(BigInt.fromI32(1));
-        if (tmp.lt(BigInt.fromI32(0))) {
-            tmp = BigInt.fromI32(0);
+        let tmp = stat.ownerCount.minus(ONE);
+        if (tmp.lt(ZERO)) {
+            tmp = ZERO;
         }
         stat.ownerCount = tmp;
     }
@@ -28,7 +31,8 @@ export const getStat = (mwadAddress: Address): Stat => {
     let stat = Stat.load(mwadAddress.toHexString());
     if (stat === null) {
         stat = new Stat(mwadAddress.toHexString());
-        stat.ownerCount = BigInt.fromI32(0);
+        stat.ownerCount = ZERO;
+        stat.assetCount = ZERO;
     }
     return stat as Stat;
 };
@@ -37,13 +41,14 @@ export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toString();
     let metaWarden = MetaWarden.load(id);
     let stat = getStat(event.address);
-    removeOwner(event.params.from, stat);
     let owner = getOwner(event.params.to, stat);
     if (metaWarden === null) {
         metaWarden = new MetaWarden(id);
 
         metaWarden.owner = event.params.to.toHexString();
         metaWarden.tokenID = event.params.tokenId;
+
+        stat.assetCount = stat.assetCount.plus(ONE);
     } else {
         metaWarden.owner = event.params.to.toHexString();
     }
@@ -51,4 +56,24 @@ export function handleTransfer(event: Transfer): void {
     metaWarden.save();
     owner.save();
     stat.save();
+
+    let prevOwner = Owner.load(event.params.from.toHexString());
+    let currentOwner = Owner.load(event.params.to.toHexString());
+
+    if (prevOwner !== null) {
+        let prevOwnerAssetCount = prevOwner.assetCount;
+        if (prevOwnerAssetCount.gt(ZERO)) {
+            prevOwnerAssetCount = prevOwnerAssetCount.minus(ONE);
+            prevOwner.assetCount = prevOwnerAssetCount;
+        }
+
+        if (prevOwnerAssetCount.le(ZERO)) {
+            removeOwner(event.params.from, stat);
+        }
+
+        prevOwner.save();
+    }
+
+    currentOwner.assetCount = currentOwner.assetCount.plus(ONE);
+    currentOwner.save();
 }
